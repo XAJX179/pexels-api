868f0b2426e99d35838acc3c1d22912423cc2b02
X_AJ_X <88792057+XAJX179@users.noreply.github.com>
2026-01-19 01:50:08 +0530

    fix(services): pexel client params

--- app/services/pexels/client.rb
+++ app/services/pexels/client.rb
@@ -1,7 +1,7 @@
 module Pexels
   module Client
     def self.get_collection(id, page = 1, per_page = 1)
-      HTTParty.get("https://api.pexels.com/v1/collections/#{id}?page=#{page}per_page=#{per_page}",
+      HTTParty.get("https://api.pexels.com/v1/collections/#{id}?page=#{page}&per_page=#{per_page}",
                     headers: { "Authorization" => Rails.application.credentials.pexels_api_key })
     end
   end
